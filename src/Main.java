d5b0bc08b369e0a3fa632f9a05fdf63c9f8dcf14
wikto <wiktor.stojek7@gmail.com>
2023-10-15 20:29:31 +0200

    committ na gałąź konfliktu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String kom = "pierwszy commit";
+        String kom = "konfliktowy commit";
         System.out.println(kom);
     }
 }
